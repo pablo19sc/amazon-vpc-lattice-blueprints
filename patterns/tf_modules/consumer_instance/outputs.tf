34935ab628efe6bfdb03fc21f5cd06c375fd2c84
Pablo Sánchez Carmona <pablosc@amazon.es>
2025-12-31 12:14:31 +0100

    adding intance IDs as outputs

--- patterns/tf_modules/consumer_instance/outputs.tf
+++ patterns/tf_modules/consumer_instance/outputs.tf
@@ -7,3 +7,8 @@ output "consumer_sg" {
   description = "Consumer VPC security group ID."
   value       = aws_security_group.instance_sg.id
 }
+
+output "ec2_instances" {
+  description = "EC2 Instance IDs"
+  value = [ for i in aws_instance.ec2_instance: i.id ]
+}

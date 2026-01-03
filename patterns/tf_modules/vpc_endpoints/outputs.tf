fba872531850f691423f2edc5dac707799961ba7
Pablo Sánchez Carmona <pablosc@amazon.es>
2026-01-03 20:52:29 +0100

    adding interface endpoint output

--- patterns/tf_modules/vpc_endpoints/outputs.tf
+++ patterns/tf_modules/vpc_endpoints/outputs.tf
@@ -2,3 +2,8 @@
  SPDX-License-Identifier: MIT-0 */
 
 # --- patterns/tf_modules/vpc_endpoints/outputs.tf ---
+
+output "interface_vpc_endpoints" {
+  description = "VPC Endpoints (Interface)"
+  value       = aws_vpc_endpoint.interface_vpc_endpoints
+}
